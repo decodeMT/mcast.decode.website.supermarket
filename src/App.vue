508313987e350d1a9e05063bd1329afc1244d1c5
algatt <alangatt@gmail.com>
2023-11-20 10:08:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex w-full h-full bg-slate-400 min-h-screen min-w-full flex-col">
+    <button @click="getProducts">Get Products</button>
+  </div>
+</template>
+
+<script setup>
+  const getProducts = async () => {};
+</script>
