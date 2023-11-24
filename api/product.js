4269ba5e23ddc194f960703b2cee15454d32a26e
algatt <alangatt@gmail.com>
2023-11-24 08:32:38 +0100

    Added error to rest

--- api/product.js
+++ api/product.js
@@ -1,26 +1,33 @@
 import products from "./products.json" assert { type: "json" };
 
 export default function (req, res) {
-  let { category, page } = req.query;
+  try {
+    let { category, page } = req.query;
 
-  page = Number(page);
-  const pageSize = 25;
+    page = Number(page);
+    const pageSize = 25;
 
-  if (!category)
-    res.status(400).json({ message: `Must specify category query` });
+    if (!category)
+      res.status(400).json({ message: `Must specify category query` });
 
-  let prods = products.filter((el) => {
-    return el.category === category;
-  });
+    let prods = products.filter((el) => {
+      return el.category === category;
+    });
 
-  const slicedProds = prods.slice(page * pageSize, page * pageSize + pageSize);
+    const slicedProds = prods.slice(
+      page * pageSize,
+      page * pageSize + pageSize
+    );
 
-  res.status(200).json({
-    products: slicedProds,
-    page: page ?? 1,
-    currentRecords: slicedProds.length,
-    offset: page * pageSize,
-    totalRecords: prods.length,
-    totalPages: Math.floor(prods.length / pageSize),
-  });
+    res.status(200).json({
+      products: slicedProds,
+      page: page ?? 1,
+      currentRecords: slicedProds.length,
+      offset: page * pageSize,
+      totalRecords: prods.length,
+      totalPages: Math.floor(prods.length / pageSize),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 }
