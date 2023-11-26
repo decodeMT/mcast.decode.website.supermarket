02d6dae60472d34fb9c9bee0db2809ca6cb2d9c7
algatt <alangatt@gmail.com>
2023-11-26 18:29:33 +0100

    Added classes

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="flex flex-col max-h-[250px] min-h-[250px] bg-slate-100 rounded-lg hover:shadow-lg overflow-hidden shadow-sm"
+    class="product flex flex-col max-h-[250px] min-h-[250px] bg-slate-100 rounded-lg hover:shadow-lg overflow-hidden shadow-sm"
   >
-    <span class="flex justify-center items-center h-[100px]">
+    <span class="flex justify-center items-center h-[100px] product_image">
       <img :src="productPlaceholder" class="h-[75px]"
     /></span>
     <span class="flex flex-col flex-1 justify-end">
       <span
-        class="text-lg font-semibold p-2 flex flex-1 items-center justify-center text-center text-slate-700"
+        class="product_name text-lg font-semibold p-2 flex flex-1 items-center justify-center text-center text-slate-700"
         >{{ product.product }}</span
       >
       <span
-        class="text-3xl flex justify-center items-center font-bold p-2 bg-slate-200 text-slate-700"
+        class="product_price text-3xl flex justify-center items-center font-bold p-2 bg-slate-200 text-slate-700"
       >
         €{{ product.salesprice.toFixed(2) }}
       </span></span
